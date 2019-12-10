e20214a7c934455e28f091ab8ea0f9c4a463ef0a
acamp1968 <56334199+acamp1968@users.noreply.github.com>
2019-12-10 15:05:35 -0500

    Further updates
    Working on header with logo - index.is the home page. adding the "s" in shtml alllows for the server-side include to show.

---
+++ public_html/_fewd/script.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+function displayHeader(header)
+
+ {
+    document.write(" <table width='100%' border='0' bgcolor='red'>");
+    document.write(" <tr>");
+    document.write(" <td width='100%' align='center'>" + pageTitle + "</td>")
+    document.write(" </tr>");
+    document.write("</table>");
+
+ }
