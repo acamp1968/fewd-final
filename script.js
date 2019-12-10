38e8bd39f06bb42e579b92933758f2cbf9aee0a9
acamp1968 <56334199+acamp1968@users.noreply.github.com>
2019-12-10 14:39:39 -0500

    Updates
    Updated files and testing

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+grunt.initConfig({
+    bake: {
+        your_target: {
+            files: {
+                "/header.html": "/header.html",
+            }
+        }
+    }
+});
