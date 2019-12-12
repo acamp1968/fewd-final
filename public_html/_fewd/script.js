b8ce484171530271d73df901f626d64f2fd2e375
acamp1968 <56334199+acamp1968@users.noreply.github.com>
2019-12-12 11:22:02 -0500

    update files

--- public_html/_fewd/script.js
+++ public_html/_fewd/script.js
@@ -1,12 +1,21 @@
 // JavaScript Document
 
-function displayHeader(header)
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
 
- {
-    document.write(" <table width='100%' border='0' bgcolor='red'>");
-    document.write(" <tr>");
-    document.write(" <td width='100%' align='center'>" + pageTitle + "</td>")
-    document.write(" </tr>");
-    document.write("</table>");
-
- }
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
